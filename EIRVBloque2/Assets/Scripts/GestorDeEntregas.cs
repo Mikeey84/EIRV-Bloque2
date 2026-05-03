fdfaed91b97840550b7ea9cb8a703f972f0cf4b9
[MarcosPRC] <marcopan@ucm.es>
2026-05-03 23:58:41 +0200

    mas sonido

--- EIRVBloque2/Assets/Scripts/GestorDeEntregas.cs
+++ EIRVBloque2/Assets/Scripts/GestorDeEntregas.cs
@@ -4,14 +4,15 @@ public class GestorDeEntregas : MonoBehaviour
 {
     [Tooltip("¿Cuántos puntos da entregar aquí?")]
     public int puntosPorEntrega = 100;
-
+    [Header("Audio")]
+    public AudioSource reproductorAudio;
     private void OnTriggerEnter(Collider otroObjeto)
     {
         if (otroObjeto.CompareTag("paquetes"))
         {
             // 1. Llamamos al cerebro central usando el Singleton
             ScoreManager.instancia.SumarPuntos(puntosPorEntrega);
-
+            reproductorAudio.Play();
             // 2. Destruimos el paquete
             Destroy(otroObjeto.gameObject);
             
